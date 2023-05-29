f31a2d1f4271147fd14f3e9988a50be5a9c03814
1871584CamachoNavarroDaniel <daniicamachoo46@gmail.com>
2023-05-29 04:27:30 -0600

    se modificó el aspecto de la interfaz de verdietas.html y verentrenamientos.html desde verdietas.php y verentrenamientos.php

--- verdietas.php
+++ verdietas.php
@@ -12,13 +12,18 @@ if ($conn->connect_error) {
     die("Error al conectar a la base de datos: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener los entrenamientos y las dietas
-$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario FROM Documentos INNER JOIN Dietas ON Documentos.IdDocumento = Dietas.DocumentoId INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario";$result = $conn->query($sql);
+// Consulta SQL para obtener los entrenamientos con categorización por sexo y nivel
+$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario, Tipocuerpo.NombreTipoCuerpo, Objetivo.NombreObjetivo FROM Documentos 
+        INNER JOIN Dietas ON Documentos.IdDocumento = Dietas.DocumentoId 
+        INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario
+        INNER JOIN Tipocuerpo ON Dietas.TipoCuerpo = Tipocuerpo.IdTipoCuerpo
+        INNER JOIN Objetivo ON Dietas.Objetivo = Objetivo.IdObjetivo";
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Crear una tabla para mostrar los resultados
     echo '<table>';
-    echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th></tr>';
+    echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th><th>Tipo de cuerpo</th><th>Objetivo</th></tr>';
 
     while ($row = $result->fetch_assoc()) {
         // Mostrar los datos de cada entrenamiento o dieta en una fila de la tabla
@@ -26,6 +31,8 @@ if ($result->num_rows > 0) {
         echo '<td>' . $row['TituloDocumento'] . '</td>';
         echo '<td>' . $row['NombreUsuario'] . '</td>';
         echo '<td>' . $row['ContenidoDocumento'] . '</td>';
+        echo '<td>' . $row['NombreTipoCuerpo'] . '</td>';
+        echo '<td>' . $row['NombreObjetivo'] . '</td>';
         echo '</tr>';
     }
 
--- verentrenamientos.php
+++ verentrenamientos.php
@@ -12,14 +12,18 @@ if ($conn->connect_error) {
     die("Error al conectar a la base de datos: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener los entrenamientos
-$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario FROM Documentos INNER JOIN Entrenamientos ON Documentos.IdDocumento = Entrenamientos.DocumentoId INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario";
+// Consulta SQL para obtener los entrenamientos con categorización por sexo y nivel
+$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario, Nivel.NombreNivel, Sexo.NombreSexo FROM Documentos 
+        INNER JOIN Entrenamientos ON Documentos.IdDocumento = Entrenamientos.DocumentoId 
+        INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario
+        INNER JOIN Nivel ON Entrenamientos.Nivel = Nivel.IdNivel
+        INNER JOIN Sexo ON Entrenamientos.Sexo = Sexo.IdSexo";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Crear una tabla para mostrar los resultados
     echo '<table>';
-    echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th></tr>';
+    echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th><th>Sexo</th><th>Nivel</th></tr>';
 
     while ($row = $result->fetch_assoc()) {
         // Mostrar los datos de cada entrenamiento en una fila de la tabla
@@ -27,6 +31,8 @@ if ($result->num_rows > 0) {
         echo '<td>' . $row['TituloDocumento'] . '</td>';
         echo '<td>' . $row['NombreUsuario'] . '</td>';
         echo '<td>' . $row['ContenidoDocumento'] . '</td>';
+        echo '<td>' . $row['NombreSexo'] . '</td>';
+        echo '<td>' . $row['NombreNivel'] . '</td>';
         echo '</tr>';
     }
 
