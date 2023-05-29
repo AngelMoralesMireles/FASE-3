8140a24dd04256370cbdae5515baeb13ff56e4e6
1871584CamachoNavarroDaniel <daniicamachoo46@gmail.com>
2023-05-28 19:33:59 -0600

    Se modificaron los archivos validar contenido.html, index.html e inicioSesion.php para que algunas paginas solo puedan ser accedidas por usuarios con un rol especifico

--- iniciosesion.php
+++ iniciosesion.php
@@ -16,14 +16,19 @@ if ($conn->connect_error) {
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Consulta SQL para verificar las credenciales del usuario
+// Consulta SQL para verificar las credenciales del usuario y obtener el rol
 $sql = "SELECT * FROM usuarios WHERE Correo = '$email' AND Contraseña = '$password'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $rolUsuario = $row['Rol'];
+
     // Usuario autenticado correctamente
     // Establecer la cookie de sesión
     setcookie('sesion_iniciada', 'true', time() + (86400 * 30), '/'); // Caduca después de 30 días (ajusta el tiempo según tus necesidades)
+    setcookie('rol_usuario', $rolUsuario, time() + (86400 * 30), '/'); // Almacena el valor del rol del usuario en una cookie
+
     // Redireccionar a "index.html"
     header("Location: index.html");
     exit();
