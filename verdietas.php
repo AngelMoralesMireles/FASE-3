db185ea0dd4a173769cecf9d84fc0e43612599c6
1871584CamachoNavarroDaniel <daniicamachoo46@gmail.com>
2023-05-29 14:06:19 -0600

    se modificaron los documentos verdietas.html y verentrenamientos.html con sus respectivos php para que se puedan ver los documentos filtrados por categorías

--- verdietas.php
+++ verdietas.php
@@ -12,12 +12,31 @@ if ($conn->connect_error) {
     die("Error al conectar a la base de datos: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener los entrenamientos con categorización por sexo y nivel
+// Obtener los parámetros del filtro desde la solicitud POST
+$tipoCuerpo = $_POST['tipoCuerpo'];
+$objetivo = $_POST['objetivo'];
+
+// Consulta SQL para obtener los documentos de dietas filtrados por tipo de cuerpo y objetivo
 $sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario, Tipocuerpo.NombreTipoCuerpo, Objetivo.NombreObjetivo FROM Documentos 
         INNER JOIN Dietas ON Documentos.IdDocumento = Dietas.DocumentoId 
         INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario
         INNER JOIN Tipocuerpo ON Dietas.TipoCuerpo = Tipocuerpo.IdTipoCuerpo
         INNER JOIN Objetivo ON Dietas.Objetivo = Objetivo.IdObjetivo";
+
+// Agregar condiciones de filtro si se seleccionaron valores
+if (!empty($tipoCuerpo)) {
+    $sql .= " WHERE Dietas.TipoCuerpo = " . $tipoCuerpo;
+}
+
+if (!empty($objetivo)) {
+    if (empty($tipoCuerpo)) {
+        $sql .= " WHERE";
+    } else {
+        $sql .= " AND";
+    }
+    $sql .= " Dietas.Objetivo = " . $objetivo;
+}
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -26,7 +45,7 @@ if ($result->num_rows > 0) {
     echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th><th>Tipo de cuerpo</th><th>Objetivo</th></tr>';
 
     while ($row = $result->fetch_assoc()) {
-        // Mostrar los datos de cada entrenamiento o dieta en una fila de la tabla
+        // Mostrar los datos de cada documento de dieta en una fila de la tabla
         echo '<tr>';
         echo '<td>' . $row['TituloDocumento'] . '</td>';
         echo '<td>' . $row['NombreUsuario'] . '</td>';
@@ -38,7 +57,7 @@ if ($result->num_rows > 0) {
 
     echo '</table>';
 } else {
-    echo 'No se encontraron entrenamientos ni dietas.';
+    echo 'No se encontraron documentos de dietas que coincidan con los criterios de filtro.';
 }
 
 // Cerrar la conexión
--- verentrenamientos.php
+++ verentrenamientos.php
@@ -12,12 +12,31 @@ if ($conn->connect_error) {
     die("Error al conectar a la base de datos: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener los entrenamientos con categorización por sexo y nivel
-$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario, Nivel.NombreNivel, Sexo.NombreSexo FROM Documentos 
+// Obtener los parámetros del filtro desde la solicitud POST
+$sexo = $_POST['sexo'];
+$nivel = $_POST['nivel'];
+
+// Consulta SQL para obtener los documentos de dietas filtrados por tipo de cuerpo y objetivo
+$sql = "SELECT Documentos.IdDocumento, Documentos.TituloDocumento, Documentos.ContenidoDocumento, Usuarios.NombreUsuario, sexo.NombreSexo, nivel.NombreNivel FROM Documentos 
         INNER JOIN Entrenamientos ON Documentos.IdDocumento = Entrenamientos.DocumentoId 
         INNER JOIN Usuarios ON Documentos.IdAutor = Usuarios.IdUsuario
-        INNER JOIN Nivel ON Entrenamientos.Nivel = Nivel.IdNivel
-        INNER JOIN Sexo ON Entrenamientos.Sexo = Sexo.IdSexo";
+        INNER JOIN Sexo ON Entrenamientos.Sexo = Sexo.IdSexo
+        INNER JOIN Nivel ON Entrenamientos.Nivel = Nivel.IdNivel";
+
+// Agregar condiciones de filtro si se seleccionaron valores
+if (!empty($sexo)) {
+    $sql .= " WHERE Entrenamientos.Sexo = " . $sexo;
+}
+
+if (!empty($nivel)) {
+    if (empty($sexo)) {
+        $sql .= " WHERE";
+    } else {
+        $sql .= " AND";
+    }
+    $sql .= " Entrenamientos.Nivel = " . $nivel;
+}
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -26,7 +45,7 @@ if ($result->num_rows > 0) {
     echo '<tr><th>Título</th><th>Autor</th><th>Contenido</th><th>Sexo</th><th>Nivel</th></tr>';
 
     while ($row = $result->fetch_assoc()) {
-        // Mostrar los datos de cada entrenamiento en una fila de la tabla
+        // Mostrar los datos de cada documento de dieta en una fila de la tabla
         echo '<tr>';
         echo '<td>' . $row['TituloDocumento'] . '</td>';
         echo '<td>' . $row['NombreUsuario'] . '</td>';
@@ -38,7 +57,7 @@ if ($result->num_rows > 0) {
 
     echo '</table>';
 } else {
-    echo 'No se encontraron entrenamientos.';
+    echo 'No se encontraron documentos de ejercicios que coincidan con los criterios de filtro.';
 }
 
 // Cerrar la conexión
